7d683107f12c2127defb761ae2671a0ac5d915a6
Vladislav <seenelse@gmail.com>
2019-09-16 00:48:22 +0300

    fix: current date

--- src/helpers/currentDate.js
+++ src/helpers/currentDate.js
@@ -1,8 +1,12 @@
 export default function () {
 	let date = new Date();
 	let minutes = date.getMinutes();
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = '0' + hours;
+	}
 	if (minutes < 10) {
 		minutes = '0' + minutes
 	}
-	return date.getHours() +':'+ minutes +' '+ date.getDate() +'.'+ (date.getMonth()+1) +'.'+ date.getFullYear();
+	return hours +':'+ minutes +' '+ date.getDate() +'.'+ (date.getMonth()+1) +'.'+ date.getFullYear();
 }
